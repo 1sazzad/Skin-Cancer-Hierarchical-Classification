bb5176f6286043e6a49e76360974169921751361
Md Sazzad Hossain <m.sazzad.h3@gmail.com>
2026-08-30 08:14:28 +0600

    feat: export shared multi-backbone builders

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -6,11 +6,19 @@ from src.models.classification_backbone import (
 )
 from src.models.densenet_baseline import build_densenet121
 from src.models.efficientnet_baseline import build_efficientnet_b0
+from src.models.shared_three_task import (
+    SUPPORTED_SHARED_ARCHITECTURES,
+    build_shared_three_task_efficientnet_b0,
+    build_shared_three_task_model,
+)
 
 
 __all__ = [
     "SUPPORTED_CLASSIFICATION_ARCHITECTURES",
+    "SUPPORTED_SHARED_ARCHITECTURES",
     "build_classification_model",
     "build_densenet121",
     "build_efficientnet_b0",
+    "build_shared_three_task_efficientnet_b0",
+    "build_shared_three_task_model",
 ]
